f2bfe21e2cab959b2528c2185a74c4212ba7be3f
Adam Titus and Jessie Waite <adam+jessie@>
2017-12-18 12:01:45 -0800

    Add router link for splash component.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,12 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SplashComponent } from './splash/splash.component';
 
-const appRoutes: Routes = [ ];
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: SplashComponent
+  }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
