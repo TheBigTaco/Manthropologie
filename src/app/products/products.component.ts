86427bb35f9760111a6513ba0e3d05a526032a98
Guest User <Guest@Epicodus-4A.local>
2017-12-20 10:59:13 -0800

    MVP functionality completed.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Manthro } from './../manthro.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
+  providers: [Manthro]
 })
 export class ProductsComponent implements OnInit {
   public category;
-  constructor() { }
+  public productId: string;
+  public productToDisplay;
+  constructor(private route: ActivatedRoute, private manthro: Manthro, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+     this.productId = urlParameters['id'];
+     console.log(urlParameters['id']);
+    });
+    this.productToDisplay = this.manthro.getProductById(this.productId);
     this.category = window.location;
+    console.log(this.productId);
   }
 
 }
